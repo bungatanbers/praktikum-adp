3a32fcc9c78d26bfc5ce6696b391ed4d61948b61
bungatanbers <163111852+bungatanbers@users.noreply.github.com>
2024-03-12 15:55:33 +0700

    Add files via upload

---
+++ bunga tugas 1.py
@@ -0,0 +1,16 @@
+panjang = float(input('masukkan panjang balok: '))
+lebar = float(input('masukkan lebar balok: '))
+tinggi =float(input('masukkan tinggi balok: '))
+sisi_kubus = float(input('masukkan sisi_kubus: '))
+
+volume_balok = panjang*lebar*tinggi
+luas_permukaan_balok = 2*(panjang*lebar+panjang*tinggi+lebar*tinggi)
+
+volume_kubus = sisi_kubus**3
+luas_permukaan_kubus = 6*sisi_kubus**2
+
+print('volume balok adalah: ', volume_balok)
+print('luas permukaan balok adalah: ', luas_permukaan_balok)
+
+print('volume kubus adalah: ', volume_kubus)
+print('luas permukaan kubus adalah: ', luas_permukaan_kubus)
