f58aff5bed26ea8c666a2aaaadff7bace480d8c7
bungatanbers <163111852+bungatanbers@users.noreply.github.com>
2024-04-02 01:25:13 +0700

    Add files via upload

---
+++ bunga tugas 4(1).py
@@ -0,0 +1,87 @@
+lanjut = "y"
+while lanjut == "y":
+    print("""
+    ----------------------------------------------------------------------
+    Hai! Selamat Datang Cream Puff Lovers CPG
+    ----------------------------------------------------------------------
+    Menu Cream Puff Gryscl
+    A. Vanilla  : Rp 5.000
+    B. Cokelat  : Rp 7.000
+    C. Greentea : Rp 8.000
+    ----------------------------------------------------------------------
+    Spesial untuk Cream Puff Lovers CPG:
+    1. Diskon sebesar 20% jika pembelian varian Vanilla lebih dari 5 pcs
+    2. Diskon sebesar 25% jika pembelian varian Cokelat lebih dari 5 pcs
+    3. Diskon sebesar 30% jika pembelian varian Greentea lebih dari 5 pcs
+    
+    nb: Jika minimal total belanja Cream Puff Lovers CPG  Rp 200.000 maka
+        mendapatkan diskon tambahan sebesar 5%
+    ----------------------------------------------------------------------
+    
+    """)
+    
+    pesan = input("Halo cream puff lovers, Anda mau pesan varian apa (A/B/C)? ").lower()
+
+    while pesan not in ['a', 'b', 'c']:
+        print("Maaf varian yang Anda inginkan tidak tersedia. Silakan pilih kembali.\n")
+        pesan = input("Anda mau pesan varian apa (A/B/C)? ").lower()
+        
+    jumlah = int(input("Berapa pcs cream puff yang ingin Anda beli? "))
+
+    
+    if pesan == "a":
+        varian = "Vanilla"
+        harga = 5000
+        if jumlah <= 5:
+            diskon = 0
+            total_harga = (harga*jumlah) - diskon
+        else:
+            diskon = (harga*jumlah) * 0.20
+            total_harga = (harga*jumlah) - diskon
+    elif pesan == "b":
+        varian = "Cokelat"
+        harga = 7000
+        if jumlah <= 5:
+            diskon = 0
+            total_harga = (harga*jumlah) - diskon
+        else:
+            diskon = (harga*jumlah) * 0.25
+            total_harga = (harga*jumlah) - diskon
+    elif pesan == "c":
+        varian = "Greentea"
+        harga = 8000
+        if jumlah <= 5:
+            diskon = 0
+            total_harga = (harga*jumlah) - diskon
+        else:
+            diskon = (harga*jumlah) * 0.30
+            total_harga = (harga*jumlah) - diskon
+    
+    if total_harga >= 200000:
+        tambahan_diskon = total_harga * 0.05
+        total_belanja = total_harga - tambahan_diskon  
+    else:
+        tambahan_diskon = 0
+        total_belanja = total_harga - tambahan_diskon
+    
+    print("----------------------------------------------------------------------")
+    print("Cream Puff rasa : ", varian)
+    print("Jumlah : ", jumlah)
+    print("Harga/pcs : ", harga)
+    print("Diskon :", diskon)
+    print("Total harga :", total_harga)
+    print("Tambahan diskon :", tambahan_diskon)
+    print("Total belanja yang harus anda bayar adalah ", total_belanja)
+    print("----------------------------------------------------------------------")
+
+
+    while True:
+        lanjut = input("Apakah Anda ingin memesan kembali? (Y/N): ").lower()
+        if lanjut == 'n':
+            break
+        elif lanjut == 'y':
+            break
+        else:
+            print("Pilihan anda tidak valid. Silakan masukkan Y untuk memesan kembali atau N untuk keluar")
+
+
