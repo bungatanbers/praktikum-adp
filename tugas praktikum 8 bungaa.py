c885c6f88152c4eb19d020b884164f1808fa5aa3
bungatanbers <163111852+bungatanbers@users.noreply.github.com>
2024-05-20 22:31:51 +0700

    Add files via upload

---
+++ tugas praktikum 8 bungaa.py
@@ -0,0 +1,58 @@
+# Fungsi untuk menambahkan data film ke file
+def tambah_film(judul, nama_penulis_skenario, nama_sutradara, tahun_rilis):
+    with open("database_film.txt", "a") as file:
+        file.write(f"{judul},{nama_penulis_skenario},{nama_sutradara},{tahun_rilis}\n")
+    print("Data film berhasil ditambahkan.")
+
+# Fungsi untuk menghapus data film dari file
+def hapus_film(judul):
+    with open("database_film.txt", "r") as file:
+        lines = file.readlines()
+    with open("database_film.txt", "w") as file:
+        for line in lines:
+            data = line.strip().split(',')
+            if data[0] != judul:
+                file.write(line)
+    print("Data film berhasil dihapus.")
+
+# Fungsi untuk menampilkan seluruh data film dari file
+def tampilkan_data_film():
+    with open("database_film.txt", "r") as file:
+        data_film = file.readlines()
+
+    if data_film:
+        print("================================================================================")
+        print("| {:<20} | {:<20} | {:<15} | {:<10} |".format("Judul Film", "Nama Penulis Skenario", "Nama sutradara", "Tahun Rilis"))
+        print("================================================================================")
+        for data in data_film:
+            judul, nama_penulis_skenario, nama_sutradara, tahun_rilis = data.strip().split(',')
+            print("| {:<20} | {:<21} | {:<15} | {:<11} |".format(judul, nama_penulis_skenario, nama_sutradara, tahun_rilis))
+        print("================================================================================")
+    else:
+        print("Database film kosong.")
+
+# Main program
+while True:
+    print("-----------------------------------")
+    print("|    KUMPULAN DATA FILM BUNGA     |")
+    print("-----------------------------------")
+    print("| Menu:                           |")
+    print("| 1. Tambah Data Film             |")
+    print("| 2. Hapus Data Film              |")
+    print("| 3. Tampilkan Semua Data Film    |")
+    print("-----------------------------------\n")
+    pilihan = input("Pilih menu (1/2/3): ")
+
+    if pilihan == "1":
+        judul = input("Masukkan judul film: ")
+        nama_penulis_skenario = input("Masukkan nama penulis skenario film: ")
+        nama_sutradara = input("Masukkan nama sutradara film: ")
+        tahun_rilis = input("Masukkan tahun rilis film: ")
+        tambah_film(judul, nama_penulis_skenario, nama_sutradara, tahun_rilis)
+    elif pilihan == "2":
+        judul = input("Masukkan judul film yang akan dihapus: ")
+        hapus_film(judul)
+    elif pilihan == "3":
+        tampilkan_data_film()
+    else:
+        print("!!!ERROR!!!Pilihan tidak valid. Silakan pilih menu yang tersedia.")
