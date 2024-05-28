da1af8e14b21fb7f4c7d59adddb124800eea82d0
bungatanbers <163111852+bungatanbers@users.noreply.github.com>
2024-05-28 23:52:44 +0700

    Add files via upload

---
+++ tugas praktikum 9 bunga.py
@@ -0,0 +1,29 @@
+from termcolor import cprint, colored 
+import os 
+os.system ('cls')
+
+for i in range (2):
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*12,"white","on_red")
+
+for i in range (1):
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    print()
+
+for i in range (2):
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_dark_grey", end=" ")
+    cprint (" "*5,"white","on_red")
+    
+for i in range(5):
+    cprint (" "*5,"white","on_red", end=" "*7)
+    cprint (" "*5,"white","on_red", end=" ")
+    cprint (" "*5,"white","on_dark_grey", end=" ")
+    cprint (" "*5,"white","on_red") 
